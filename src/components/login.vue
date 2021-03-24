5b0e3f7cb4519e116492dcc9e18b3145b73598c4
ltf-space <1036285771@qq.com>
2021-03-24 22:41:27 +0800

    first登录框

---
+++ src/components/login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'login',
+  components:{},
+  data(){
+    return {
+    }
+  },
+}
+</script>
+<style scoped>
+.login_container{
+  height: 100%;
+  background-color: #2b4b6b;
+}
+.login_box{
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%,-50%);
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//导入全局css样式
+import './assets/css/global.css'
+
+//导入element组件库
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+
 Vue.use(ElementUI);
 Vue.config.productionTip = false
 
