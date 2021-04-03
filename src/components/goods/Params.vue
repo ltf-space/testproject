7bf292e3da7867a1644ef1b1a1e8837bd5da3763
ltf-space <1036285771@qq.com>
2021-04-03 21:37:29 +0800

    二次提交

--- src/components/goods/Params.vue
+++ src/components/goods/Params.vue
@@ -34,7 +34,8 @@
       <el-tabs v-model="activeName" @tab-click="handleTapClick">
         <!-- 添加动态参数模板 -->
         <el-tab-pane label="动态属性" name="many">
-          <el-button type='primary' size='mini' :disabled='isBtnDisabled'>添加参数</el-button>
+          <el-button type='primary' size='mini' :disabled='isBtnDisabled'
+          @click="addDialogVisble = true">添加参数</el-button>
           <!-- 动态分类参数表格 -->
           <el-table :data="manyTableData" stripe border style="width: 100%">
             <el-table-column type='expand'></el-table-column>
@@ -42,16 +43,19 @@
             <el-table-column type='index' label="#" ></el-table-column>
             <el-table-column prop="attr_name" label="参数名称" ></el-table-column>
             <el-table-column label="操作" >
-              <template>
-                <el-button type="primary" icon="el-icon-edit" size='mini'></el-button>
-                <el-button type="danger" icon="el-icon-delete" size='mini'></el-button>
+              <template slot-scope="scope">
+                <el-button type="primary" icon="el-icon-edit" size='mini'
+                @click="showEditDialog(scope.row.attr_id)"></el-button>
+                <el-button type="danger" icon="el-icon-delete" size='mini'
+                @click="removeDialog(scope.row.attr_id)"></el-button>
               </template>
             </el-table-column>
           </el-table>
         </el-tab-pane>
         <!-- 添加静态参数模板 -->
         <el-tab-pane label="静态属性" name="only">
-          <el-button type='primary' size='mini' :disabled='isBtnDisabled'>添加属性</el-button>
+          <el-button type='primary' size='mini' :disabled='isBtnDisabled'
+          @click="addDialogVisble = true">添加属性</el-button>
           <!-- 静态分类参数表格 -->
           <el-table :data="onlyTableData" stripe border style="width: 100%">
             <el-table-column type='expand'></el-table-column>
@@ -59,15 +63,51 @@
             <el-table-column type='index' label="#" ></el-table-column>
             <el-table-column prop="attr_name" label="参数名称" ></el-table-column>
             <el-table-column label="操作" >
-              <template>
-                <el-button type="primary" icon="el-icon-edit" size='mini'></el-button>
-                <el-button type="danger" icon="el-icon-delete" size='mini'></el-button>
+              <template slot-scope="scope">
+                <el-button type="primary" icon="el-icon-edit" size='mini'
+                @click="showEditDialog(scope.row.attr_id)"></el-button>
+                <el-button type="danger" icon="el-icon-delete" size='mini'
+                @click="removeDialog(scope.row.attr_id)"></el-button>
               </template>
             </el-table-column>
           </el-table>
         </el-tab-pane>
       </el-tabs>
     </el-card>
+
+    <!-- 添加动态/静态属性对话框 -->
+    <el-dialog
+      :title=" '添加' + titleText"
+      :visible.sync="addDialogVisble"
+      width="50%"
+      @close='addDialogClosed'>
+      <el-form :model="addParamsForm" :rules="addParamsFormRules" ref="addParamsFormRef" label-width="100px">
+        <el-form-item :label="titleText" prop="attr_name">
+          <el-input v-model="addParamsForm.attr_name"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addDialogVisble = false">取 消</el-button>
+        <el-button type="primary" @click="addParams">确 定</el-button>
+      </span>
+    </el-dialog>
+
+    <!-- 添加编辑对话框 -->
+     <el-dialog
+      :title=" '修改'+titleText"
+      :visible.sync="editDialogVisble"
+      width="50%"
+      @close='editDialogClosed'>
+      <el-form :model="editParamsForm" :rules="addParamsFormRules" ref="editParamsFormRef" label-width="100px">
+        <el-form-item :label="titleText" prop="attr_name">
+          <el-input v-model="editParamsForm.attr_name"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="editDialogVisble = false">取 消</el-button>
+        <el-button type="primary" @click="editParams">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -94,7 +134,23 @@ export default {
       //存储动态参数数据
       manyTableData:[],
       //存储静态参数数据
-      manyTableData:[]
+      onlyTableData:[],
+      // 控制添加属性对话框隐藏
+      addDialogVisble:false,
+      // 存储动态/静态数据
+      addParamsForm:{},
+      // 验证规则
+      addParamsFormRules:{
+        attr_name:[
+          { required:true,message:'请输入参数名称',trigger:'blur'}
+        ]
+      },
+      // 控制编辑对话框隐藏
+      editDialogVisble:false,
+      // 存储编辑后的数据
+      editParamsForm:{}
+       
+      
     }
   },
   created(){
@@ -113,6 +169,13 @@ export default {
         return this.selectedKeys[2]
       }
       return
+    },
+    titleText(){//动态或静态属性文本
+      if(this.activeName === 'many'){
+        return '动态参数'
+      }else{
+        return '静态属性'
+      }
     }
   },
   methods:{
@@ -147,6 +210,54 @@ export default {
       }else{//获取静态参数数据
         this.onlyTableData = res.data
       }
+    },
+    addDialogClosed(){//监听对话框关闭
+      this.$refs.addParamsFormRef.resetFields()
+    },
+    addParams(){//点击添加属性确定按钮
+      this.$refs.addParamsFormRef.validate(async valid => {
+        if(!valid)return
+        const {data:res} = await this.$axios.post(`categories/${this.cateId}/attributes`,
+        { attr_name:this.addParamsForm.attr_name,attr_sel:this.activeName })
+        if(res.meta.status !== 201)return this.$message.error('添加失败')
+        this.$message.success('添加成功')
+        this.getParamsData()
+        this.addDialogVisble = false
+        // console.log(res)
+      })
+    },
+    async showEditDialog(id){//点击编辑按钮触发
+      const {data:res} = await this.$axios.get(`categories/${this.cateId}/attributes/${id}`,{params:{attr_sel:this.activeName}})
+      if(res.meta.status !== 200)return this.$message.error('获取信息失败')
+      this.editParamsForm = res.data
+      // console.log(res)
+      this.editDialogVisble = true
+    },
+    editParams(){//点击编辑对话框确定按钮
+      this.$refs.editParamsFormRef.validate(async valid => {
+        if(!valid)return
+        const {data:res} = await this.$axios.put(`categories/${this.cateId}/attributes/${this.editParamsForm.attr_id}`,
+        {attr_name:this.editParamsForm.attr_name,attr_sel:this.activeName})
+        console.log(res)
+        this.getParamsData()
+        this.editDialogVisble = false
+      })
+    },
+    editDialogClosed(){//监听对话框关闭
+      this.$refs.editParamsFormRef.resetFields()
+    },
+    async removeDialog(id){//点击删除按钮，删除参数
+      const resultConfirm = await this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).catch(err => err)
+      if(resultConfirm !== 'confirm')return this.$message.info('已取消删除')
+      const {data:res} = await this.$axios.delete(`categories/${this.cateId}/attributes/${id}`)
+      console.log(res)
+      if(res.meta.status !== 200)return this.$message.error('删除数据失败')
+      this.$message.success('删除参数成功')
+      this.getParamsData()
     }
   }
 }
